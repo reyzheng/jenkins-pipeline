277c864d611240c2179fd621d4576fc6872451c1
reyzheng <reyzheng@gmail.com>
2023-03-01 22:04:33 +0800

    Update cue.py

--- cue.py
+++ cue.py
@@ -1,1 +1,10 @@
+import json
 
+fpGlobal = open('settings/global_config.json')
+cfgGlobal = json.load(fpGlobal)
+  
+for stage in cfgGlobal['stages']:
+    print(stage)
+  
+# Closing file
+fpGlobal.close()
