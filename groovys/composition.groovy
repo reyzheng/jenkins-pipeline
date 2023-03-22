855ba6e5cbcd2040c03368f87d0ea0bee87b66dc
reyzheng <reyzheng@gmail.com>
2023-03-22 19:06:00 +0800

    Update composition.groovy

--- groovys/composition.groovy
+++ groovys/composition.groovy
@@ -11,8 +11,12 @@ def init(stageName) {
     def config = utils.commonInit(stageName, defaultConfigs)
     config.settings.parallel_parameters = utils.extractParallelParameters(config.settings.parallel_parameters)
     config.settings.parallel_excludes = utils.extractParameters(config.settings.parallel_excludes)
+    dir ('.pf-composition') {
+        writeJSON file: 'temporal.json', json: config.settings
+        stash name: "pf-${stageName}-config", includes: 'temporal.json'
+    }
 
-    return config
+    return null
 }
 
 def func(pipelineAsCode, configs, preloads) {
