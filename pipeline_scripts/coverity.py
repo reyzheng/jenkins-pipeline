02c11254086df31da29359c20933afc432b361b4
Rey Cheng <reyzheng@gmail.com>
2023-03-25 14:02:37 +0800

    update
    Signed-off-by: Rey Cheng <reyzheng@gmail.com>

--- pipeline_scripts/coverity.py
+++ pipeline_scripts/coverity.py
@@ -7,9 +7,10 @@ def coverity(configFile):
 
 if __name__ == '__main__':
     try:
-        opts, args = getopt.getopt(argv, "c:")  
-    except:
-        print("Error")
+        opts, args = getopt.getopt(sys.argv[1:], "c:", ["config="])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
   
     for opt, arg in opts:
         if opt in ['-c']:
