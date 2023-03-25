01a1bb50cfd47f288a5568fc43922cddb630d6c6
Rey Cheng <reyzheng@gmail.com>
2023-03-25 21:20:47 +0800

    update
    Signed-off-by: Rey Cheng <reyzheng@gmail.com>

--- groovys/common_stage.groovy
+++ groovys/common_stage.groovy
@@ -86,7 +86,7 @@ def func(pipelineAsCode, configs, actionConfig) {
                         sh "mv -f ../${dstFile} ."
                     }
                     else {
-                        bat "move ..\\.script\\\"${dstFile}\" . /Y"
+                        bat "move /Y ..\\.script\\\"${dstFile}\" ."
                     }
                     if (scriptTypes[i] == "source") {
                         sh ". " + dstFile
