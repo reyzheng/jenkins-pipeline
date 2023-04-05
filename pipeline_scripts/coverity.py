cc01a5245b93de7996bf087bbe0e6b7257f6b2b0
reyzheng <reyzheng@gmail.com>
2023-04-05 15:13:03 +0800

    Update coverity.py

--- pipeline_scripts/coverity.py
+++ pipeline_scripts/coverity.py
@@ -1,106 +1,49 @@
-# coverity.py
-import getopt
-import sys
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-def coverity(configFile):
-    fpJSON = open(configFile)
-    coverityConfig = json.load(f)
+import os
+import getopt, sys
+import subprocess as sb
 
-    for i in range(0, len(coverityConfig["type"])):
-        # TODO: check scriptTypes
-        
-        if (scriptAction == true) {
-            if (coverityConfigScripted.expressions[i] && coverityConfigScripted.expressions[i] != "") {
-                def expr = evaluate(coverityConfigScripted.expressions[i])
-                if (expr == false) {
-                    print "skip ${i}th script"
-                    continue
-                }
-            }
-        }
+def getDiffFiles(src):
+    os.chdir(src)
+    # git log --format=\"%H\" -n 2
+    commitIds = []
+    cmdCommitIds = sb.Popen(['git', 'log' ,'--format=\"%H\"', '-n', '2'], stdout=sb.PIPE)
+    while True:
+        line = cmdCommitIds.stdout.readline()
+        if not line:
+            break
+        commitIds.append(line.decode("utf-8") .strip()[1:-1])
 
-        def buildDir = ""
-        if (scriptAction == false && buildDirs[i] != null) {
-            buildDir = buildDirs[i]
-        }
-        def coverityConfigIdx = i
-        if (coverityConfigScripted.buildmapping == "manytoone") {
-            coverityConfigIdx = 0
-        }
-        def secondScanCleanDir = coverityConfigScripted.coverity_clean_builddir
-        if (coverityConfigScripted.coverity_analyze_parent == "prev" ||
-                coverityConfigScripted.coverity_analyze_parent == "branch" ||
-                coverityConfigScripted.coverity_analyze_parent == "custom") {
-            def varname
-            secondScanCleanDir = false
-            if (env.BUILD_BRANCH) {
-                varname = "${env.BUILD_BRANCH}_SOURCE_DIR${i}"
-            }
-            else {
-                varname = "SOURCE_DIR${i}"
-            }
-            def sourceDst = env."${varname}"
+    cmdDiffs = sb.Popen(['git', 'diff' ,'--submodule=diff', '{}..{}'.format(commitIds[1], commitIds[0])], stdout=sb.PIPE)
+    if os.name == "posix":
+        nameDiffs = sb.check_output(('grep', 'diff --git'), stdin=cmdDiffs.stdout)
+    else:
+        nameDiffs = sb.check_output(('findstr', '/l', 'diff --git'), stdin=cmdDiffs.stdout)
+    cmdDiffs.wait()
+    lines = nameDiffs.decode("utf-8").splitlines()
+    for line in lines:
+        tokens = line.split()
+        print("file('{}')".format(tokens[3][2:]))
 
-            if (coverityConfigScripted.coverity_analyze_parent == "custom") {
-                dir(".gitscript") {
-                    unstash name: 'stash-coverity-checkout-parent'
-                    unstash name: 'stash-script-bdsh'
-                }
-                sh """
-                    sh .gitscript/bdsh.sh .gitscript/checkout-parent.sh
-                """
-            }
-            else {
-                dir(".gitscript") {
-                    unstash name: "git-label-submodules"
-                    unstash name: "git-checkout-parent"
-                }
-                sh """
-                    sh .gitscript/git-label-submodules.sh ${sourceDst}
-                    sh .gitscript/git-checkout-parent.sh ${sourceDst} ${coverityConfigScripted.coverity_analyze_parent}
-                """
-            }
-            dir(buildDir) {
-                coverityConfigScripted.refParent = true
-                coverity_scan(coverityConfigScripted, coverityPreloads, scriptTypes[i], scriptContents[i], coverityConfigIdx, scriptAction)
-            }
-            if (coverityConfigScripted.coverity_analyze_parent == "custom") {
-                dir(".gitscript") {
-                    unstash name: 'stash-coverity-checkout-parent'
-                    unstash name: 'stash-script-bdsh'
-                }
-                sh """
-                    sh .gitscript/bdsh.sh .gitscript/checkout-current.sh
-                """
-            }
-            else {
-                dir(".gitscript") {
-                    unstash name: "git-checkout-parent"
-                }
-                sh """
-                    sh .gitscript/git-checkout-parent.sh ${sourceDst} forward
-                """
-            }
-        }
-        dir(buildDir) {
-            coverityConfigScripted.refParent = false
-            coverityConfigScripted.coverity_clean_builddir = secondScanCleanDir
-            coverity_scan(coverityConfigScripted, coverityPreloads, scriptTypes[i], scriptContents[i], coverityConfigIdx, scriptAction)
-        }
-    }
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv[1:], 'c:s:v', ["command=", "source=", "version"])
+    except getopt.GetoptError:
+        sys.exit()
 
+    for name, value in opts:
+        if name in ('-v', '--version'):
+            print("0.1")
+            sys.exit(0)
+        elif name in ('-c', '--command'):
+            COMMAND = value
+        elif name in ('-s', '--source'):
+            SRC = value
 
-    fpJSON.close()
+    if COMMAND == "PF_DIFF_PREV":
+        getDiffFiles(SRC)
 
 if __name__ == '__main__':
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "c:", ["config="])
-    except getopt.GetoptError as err:
-        print(err)
-        sys.exit(2)
-  
-    for opt, arg in opts:
-        if opt in ['-c']:
-            config = arg
-
-    coverity(config)
+    main(sys.argv)
