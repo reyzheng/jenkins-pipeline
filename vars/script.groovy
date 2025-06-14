f22b57c89d45105f0515498fc43f01c8b7f7c534
Rey Cheng <reyzheng@gmail.com>
2025-06-14 20:34:52 +0800

    env
    Signed-off-by: Rey Cheng <reyzheng@gmail.com>

--- vars/script.groovy
+++ vars/script.groovy
@@ -33,7 +33,7 @@ def buildEnv(toolbox) {
 
 // actionConfig
 def call(stageName) {
-    def utils = load "${PF_ROOT}/utils.groovy"
+    def utils = load "${env.PF_ROOT}/utils.groovy"
     def configs = readJSON file: "${env.PF_ROOT}/settings/${stageName}_config.json"
     def underUnix = isUnix()
     def validScriptTypes = ["inline", "file", "source", "groovy"]
