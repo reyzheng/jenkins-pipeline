2a3bfb1c58a7b79f642c98913eda8d60cb01e90a
Rey Cheng <reyzheng@gmail.com>
2025-06-14 20:40:51 +0800

    remove env
    Signed-off-by: Rey Cheng <reyzheng@gmail.com>

--- vars/script.groovy
+++ vars/script.groovy
@@ -32,7 +32,7 @@ def buildEnv(toolbox) {
 }
 
 // actionConfig
-def call(stageName, env) {
+def call(stageName) {
     def utils = load "${env.PF_ROOT}/utils.groovy"
     def configs = readJSON file: "${env.PF_ROOT}/settings/${stageName}_config.json"
     def underUnix = isUnix()
