608753509bba066ce0d9eaae2be57d32200e23c0
Rey Cheng <reyzheng@gmail.com>
2025-06-17 22:27:21 +0800

    stash
    Signed-off-by: Rey Cheng <reyzheng@gmail.com>

--- utils.groovy
+++ utils.groovy
@@ -171,12 +171,7 @@ def unstashPipelineFramework() {
                 }
             }
             print "unstash stash-pf-framework finished"
-            //unstash name: "stash-pf-config"
-            copyArtifacts(
-                projectName: "${JOB_NAME}",
-                filter: 'Jenkinsfile*,settings/**,scripts/**,utils.groovy,groovys/**,pipeline_scripts/**,templates/**,rtk_coverity/**,vendor/**',
-                selector: specific("${BUILD_NUMBER}")
-            )
+            unstash name: "stash-pf-config"
             print "unstash stash-pf-config finished (utils)"
         }
     }
